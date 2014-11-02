fc6c91635a7e3e1af7cc66156a5f14e341ae4dbd
Utkarsh Surwade <utkarsh77777@gmail.com>
2014-11-02 22:50:34 +0530

    Create add_two_nos.cpp

---
+++ add_two_nos.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int x,y,result;
+  cout<<"plz enter 1st no.  - ";
+  cin>>x;
+  cout<<"plz enter 2nd no. -";
+  cin>>y;
+  result = x +y;
+
+  cout<<"the sum of two nos is:"<<result;
+  return 0;
+}
+
