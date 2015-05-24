a000880c645c38d064e20606269b4dda19bf43bd
Utkarsh Surwade <utkarsh77777@gmail.com>
2015-05-24 23:41:07 +0530

    Update 1_hello_world.cpp

--- 1_hello_world.cpp
+++ 1_hello_world.cpp
@@ -11,6 +11,6 @@ int main()//main function
     cout/*output for the console*/<</*'put two' operator(must be there after "cout")*/"Hello world!"/*string(data type)*/;//end of statement
     //(just like fullstop.)
 
-    return 0;//return value for function(return value 0 unless theres a problem)[when return value is zero then the function is running properly]
+    return 0;//return value for function (return value 0 unless theres a problem)[when return value is zero then the function is running properly]
 
 }//ending the body of main function
