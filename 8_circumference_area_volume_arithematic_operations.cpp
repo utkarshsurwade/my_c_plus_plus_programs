edfcf67c1dc222a791cedfcf298085b22b7ddab7
Utkarsh Surwade <utkarsh77777@gmail.com>
2015-05-28 17:58:02 +0530

    created

---
+++ 8_circumference_area_volume_arithematic_operations.cpp
@@ -0,0 +1,41 @@
+/*find the area,circumference,volume of a circle and use addition,multiplication ,subtraction,division,modulous/remainder operators
+to solve a given simple equation
+
+*/
+
+#include <iostream>
+
+#include <cmath>/*This library provides advanced mathematical function to use in our programes such as fmod(,)  ,  pow(,)  ,  abs().....
+for finding absolut value and      sqrt().....for finding underroot of a value etc.
+ */
+using namespace std;
+
+int main()
+{
+    double radius,num1,num2;
+
+    const double pi = 3.14159265 ;
+
+    cout << "what is the radius of the circle?" << endl;
+
+    cin>>radius;
+
+    cout<<"\nThen area of the circle is:"<<pi * pow(radius,2)<<endl;
+    cout<<"Then the circumference of the circle is:"<<2*pi*radius<<endl;
+    cout<<"Then the volume of the circle is:"<<(4/3.0)/*while dividing ,to get quotient or numerator in decimal form ,we have to make one part of the function in decimal form[here "3.0"]*/*pi*pow(radius,3)<<endl;
+    cout<<"then the surface area of the circle is:"<<4*pi*pow(radius,2)<<endl;
+
+    cout<<"\nEnter two numbers:";
+    cin>>num1>>num2;
+    cout<<"Therefore the addition,subtraction,multiplication,division & modulous/remainder of the two nos. is:\n\n";
+    cout<<"addition = "<<num1 + num2;
+    cout<<"\nsubtraction = "<<num1 - num2;
+    cout<<"\nmultiplication = "<<num1 * num2;
+    cout<<"\ndivision = "<<num1 /num2;//the result be the quotient
+  /*cout<<"\nremainder = "<<num1 % num2; (modulous operator works with whole numbers only,in this case
+    we have taken double [folating point]as our data type).it is defined for whole numbers only*/
+    cout<<"\nremainder = "<<fmod(num1,num2)<<"\n\n";//to overcome the above problem,we use fmod function in the cmath library
+
+
+    return 0;
+}
---
+++ 9_if_elseif_else.cpp
@@ -0,0 +1,148 @@
+/*GRADES OF STUDENT ACCORDING TO MARKS OBTAINEDE FROM 100 IS ~
+
+grading scale
+
+97 - 100    A+
+93 - 96     A
+90 - 92     A-
+
+87 - 89     B+
+83 - 86     B
+80 - 82     B-
+
+77 - 79     C+
+73 - 76     C
+70 - 72     C-
+
+67 - 69     D+
+63 - 66     D
+60 - 62     D-
+
+0 - 59      F
+
+(if , else if , else statements are used primarily when we have a structured decision to make[if statement is basically a control flow statement])
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+int marks;
+string grade;
+
+cout<<"how many marks have u got?\n";
+
+cin >> marks;
+
+cout<<"then your grade is:";
+
+if ((marks>=97)/*relational expression*/ &&/* "and" logical operator(there are many logical operators such as ~ "and" , "or" , "not" etc)*/ (marks<=100))
+
+{
+    cout <<"A+\n";
+
+}
+else if((marks>=93) && (marks<=96))
+
+
+{
+    cout << "A\n";
+
+
+
+    }
+
+    else if ((marks>=90) && (marks <=92))
+
+    {
+        cout<<"A-\n";
+
+
+    }
+
+
+     else if ((marks>=87) && (marks<=89))
+
+    {
+        cout<<"B+\n";
+
+
+    }
+
+
+      else if ((marks>=83) && (marks<=86))
+
+    {
+        cout<<"B\n";
+
+
+    }
+
+    else if ((marks>=80)&& (marks<=82))
+
+    {
+     cout<<"B-\n";
+
+    }
+
+
+    else if ((marks>=77) && (marks<=79))
+
+    {
+        cout<<"C+\n";
+
+
+    }
+
+    else if ((marks>=73)&& (marks<=76))
+
+    {
+     cout<<"C\n";
+
+    }
+
+else if ((marks>=70) && (marks<=72))
+
+    {
+        cout<<"C-\n";
+
+
+    }
+
+    else if ((marks>=67)&& (marks<=69))
+
+    {
+    cout<<"D+\n";
+
+    }
+
+
+    else if ((marks>=63) && (marks<=66))
+
+    {
+        cout<<"D\n";
+
+
+    }
+
+    else if ((marks>=60)&& (marks<=62))
+
+    {
+    cout<<"D-\n";
+
+    }
+
+    else
+
+    {
+        cout<<"F\n";
+
+
+    }
+
+    return 0;
+}
