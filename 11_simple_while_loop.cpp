922ad87b0125abf7b677f01db7999fb3de7ed4d2
Utkarsh Surwade <utkarsh77777@gmail.com>
2015-05-30 00:55:37 +0530

    created

---
+++ 11_simple_while_loop.cpp
@@ -0,0 +1,22 @@
+//wap to display "hello world" ten times using while loop
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int count = 1;//initialize the loop control variable outside the the while statement
+    while (count <= 10)//relational expression[further count in this case is called loop control variable]
+
+    {
+
+    cout<<count<< ": ";//statement to make the program more neat
+    cout << "Hello world!\n" << endl;//body or statement that does some type of work
+
+
+    ++/*increment(keep on incrementing "count" until its equal to 10)*/count;//some statement(maybe increment "++" or decrement "--")that modifies the loop control variable so that makes the relational expression false
+    }
+    return 0;
+
+}
