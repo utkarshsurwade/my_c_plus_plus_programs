761280c49fbc35ff5a05af31c63519fcd2922e84
Utkarsh Surwade <utkarsh77777@gmail.com>
2015-06-11 14:43:13 +0530

    Update 4_size_of_data_type.cpp

--- 4_size_of_data_type.cpp
+++ 4_size_of_data_type.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int main()
 {
-    cout << sizeof(float)/*fuction to check the size of data type(int,double,float etc) in  bytes [this function is compiler dependent]*/<< endl;
+    cout << sizeof(float)/*fuction to check the size of data type(int,double,float etc) in  bytes 
+                         [this function is compiler dependent]*/;
+    cout<<endl;
     return 0;
 }
