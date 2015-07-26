c958c50fef4d42703b66576223e735c102c7409f
Utkarsh Surwade <utkarsh77777@gmail.com>
2015-07-26 18:18:13 +0530

    Update 012_count_controlled_while_loop.cpp

--- 012_count_controlled_while_loop.cpp
+++ 012_count_controlled_while_loop.cpp
@@ -1,5 +1,6 @@
 //there are two types of while loop ~ 1: count controlled while loop (loop control variable keeps track of output)
-//and 2: sentinel control while loop (loop control variable does'nt keep track of output instead it looks for a certain value in order to stop the loop)
+//and 2: sentinel control while loop 
+//(loop control variable does'nt keep track of output instead it looks for a certain value in order to stop the loop)
 //wap using while loop to show multiplication using addition
 #include <iostream>
 using namespace std;
@@ -16,9 +17,11 @@ int main()
 
     while (no <=num2)//loop control variable "no" which is in the form of a relational statement
     {
-        product += num1;//body or statement that does some type of work(take the value of num1 and add it into the current value of product and keep on doing this until the loop stops)
+        product += num1;//body or statement that does some type of work
+           //(take the value of num1 and add it into the current value of product and keep on doing this until the loop stops)
 
-        cout<<num1<<" + "<<no*num1-num1<<"="<<product<<"\n";//so that the program's output looks neat(in order to verify the loop)[in order to show the steps]
+        cout<<num1<<" + "<<no*num1-num1<<"="<<product<<"\n";//so that the program's output looks neat
+                                                           //(in order to verify the loop)[in order to show the steps]
 
         ++no;//increment "no" in order to stop the loop
     }
