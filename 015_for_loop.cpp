2721a1dc2c6a117ae14f5cdab63b60e6d77a87bd
Utkarsh Surwade <utkarsh77777@gmail.com>
2015-07-26 17:55:57 +0530

    Update 015_for_loop.cpp

--- 015_for_loop.cpp
+++ 015_for_loop.cpp
@@ -16,10 +16,15 @@ int main()
     cin >> rate;
     cout<<"how many years to calculate?:";
     cin>>years;
-    for(int i =1;/*initialization*/i<=years;/*relation expression(in order to test that loop controle variable)*/++i/*loop controle variable modification(increment ++ or decrement --)*/)
+    for(int i =1;/*initialization*/i<=years;/*relation expression(in order to test that loop controle variable)*/++i)
+                            /*in above for syntax '++i' is loop controle variable modification(increment ++ or decrement --)*/
     {
-        balance += rate/100*balance;//body or statement that does some type of work(take the value of rate (in %) and multiply it into the current value of balance and keep on doing this until the loop stops)
-        cout<<"\nIn year number "<<i<<" balance " <<"= "<< balance;//so that the program's output looks neat(in order to verify the loop)[in order to show the steps]
+        balance += rate/100*balance;//body or statement that does some type of work
+        // (take the value of rate (in %) and multiply it into the current value of balance 
+        // and keep on doing this until the loop stops)
+        
+        cout<<"\nIn year number "<<i<<" balance " <<"= "<< balance;//so that the program's output looks neat
+                                                                  //(in order to verify the loop)[in order to show the steps]
     }
     cout<<"\n\n"<<"After "<< years << " years,your current balance will be "<<balance<<"\n\n";
     return 0;
