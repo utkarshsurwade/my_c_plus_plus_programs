5e5b684df1a2048d3a365ea342eaec17cb2f28f7
Utkarsh Surwade <utkarsh77777@gmail.com>
2015-07-26 17:33:34 +0530

    Update 008_circumference_area_volume_arithematic_operations.cpp

--- 008_circumference_area_volume_arithematic_operations.cpp
+++ 008_circumference_area_volume_arithematic_operations.cpp
@@ -3,7 +3,7 @@ modulous/remainder operators to solve a given simple equation.*/
 
 #include <iostream>
 
-#include <cmath>/*This library provides advanced mathematical function to use in our programes such as fmod(,)  ,  pow(,)  ,  abs().....
+#include <cmath>/*This library provides advanced mathematical function to use in our programes such as fmod(,) {special modulous/remainder finction}  ,  pow(,)  ,  abs().....
 for finding absolute value and      sqrt().....for finding underroot of a value etc.
  */
 using namespace std;
@@ -20,7 +20,7 @@ int main()
 
     cout<<"\nThen area of the circle is:"<<pi * pow(radius,2)<<endl;
     cout<<"Then the circumference of the circle is:"<<2*pi*radius<<endl;
-    cout<<"Then the volume of the circle is:"<<(4/3.0)/*while dividing ,to get quotient or numerator in decimal form ,we have to make one part of the function in decimal form[here "3.0"]*/*pi*pow(radius,3)<<endl;
+    cout<<"Then the volume of the circle is:"<<(4/3.0)/*while dividing ,in order to get quotient in decimal form ,we have to make one part of the function in decimal form[here "3.0"]*/*pi*pow(radius,3)<<endl;
     cout<<"then the surface area of the circle is:"<<4*pi*pow(radius,2)<<endl;
 
     cout<<"\nEnter two numbers:";
