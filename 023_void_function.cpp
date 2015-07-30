262a07f0b8f1dfb73eb8f4f5e98edd0199c4fc58
utkarshsurwade <utkarsh77777@gmail.com>
2015-07-31 00:16:53 +0530

    created

---
+++ 023_void_function.cpp
@@ -0,0 +1,18 @@
+//void function does not return a value.
+//you use it primarily to perform some set of operations without expecting it to return a value as a result of those operations
+
+#include <iostream>
+
+using namespace std;
+
+void /*void is the substitute for the return data type*/ heading ()//parameter list
+{
+    cout<<"wazzup";
+}
+
+int main()
+{
+    heading ();
+    cout << endl;
+    return 0;
+}
